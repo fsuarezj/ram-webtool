d239d38f2d15066ae9ade181731337ce3a244f9a
fsuarezj <fsuarezj@gmail.com>
2016-06-26 01:25:02 +0200

    First iteration new schema, not tested

--- docroot/WEB-INF/src/org/lrc/liferay/toolbuilder/bean/InstanceBacking.java
+++ docroot/WEB-INF/src/org/lrc/liferay/toolbuilder/bean/InstanceBacking.java
@@ -6,11 +6,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
-import org.lrc.liferay.toolbuilder.model.WrapperStep;
-import org.lrc.liferay.toolbuilder.service.persistence.ToolInstanceUtil;
-import org.lrc.liferay.toolbuilder.service.persistence.WrapperStepUtil;
+import org.lrc.liferay.toolbuilder.ToolInstance;
 
-import com.liferay.faces.portal.context.LiferayFacesContext;
 import com.liferay.portal.kernel.exception.SystemException;
 
 @ManagedBean
@@ -20,24 +17,9 @@ public class InstanceBacking extends AbstractBaseBean {
 	@ManagedProperty(name = "toolSession", value = "#{toolSession}")
 	protected ToolSession toolSession;
 	
-	public String add() throws ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InstantiationException, NoSuchMethodException, SecurityException, InvocationTargetException {
+	public String add() throws ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InstantiationException, NoSuchMethodException, SecurityException, InvocationTargetException, SystemException {
 		System.out.println("Adding a new Tool Instance");
-		org.lrc.liferay.toolbuilder.model.ToolInstance toolInstance = ToolInstanceUtil.create(0L);
-		LiferayFacesContext liferayFacesContext = LiferayFacesContext.getInstance();
-		toolInstance.setGroupId(liferayFacesContext.getScopeGroupId());
-		toolInstance.setCompanyId(liferayFacesContext.getCompanyId());
-		toolInstance.setUserId(liferayFacesContext.getUserId());
-		
-		WrapperStep wrapperStep = WrapperStepUtil.create(0L);
-		wrapperStep.setSequential(true);
-		wrapperStep.setGroupId(liferayFacesContext.getScopeGroupId());
-		wrapperStep.setCompanyId(liferayFacesContext.getCompanyId());
-		wrapperStep.setUserId(liferayFacesContext.getUserId());
-		wrapperStep.createSteps();
-		wrapperStep.setStepsNumber(5);
-		wrapperStep.setCurrentStep(0);
-		
-		toolInstance.setWrapperStep(wrapperStep);
+		ToolInstance toolInstance = toolSession.createToolInstance();
 		
 		this.toolSession.setConfiguringInstance();
 		return this.toolSession.selectToolInstance(toolInstance);
